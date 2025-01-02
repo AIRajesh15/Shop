daac881582e1288dc2c99b500587943cfa3f6fbd
AIRajesh15 <rvasita@aakashinfosoft.com>
2025-01-02 18:26:23 +0530

    MyShop Setup

---
+++ Solution1/MyShop.WebUi.Tests/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyShop.WebUi.Tests.MvcApplication" Language="C#" %>
---
+++ Solution1/MyShop.WebUi.Tests/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyShop.WebUi.Tests.Startup))]
+namespace MyShop.WebUi.Tests
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
